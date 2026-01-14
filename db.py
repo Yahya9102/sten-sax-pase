563a4a276675ac16d05522b0c818171d9a203a3a
Yahya9102 <yaya_hussein@hotmail.com>
2026-01-14 17:15:31 +0100

    changed typing errors in mysql.yml

--- db.py
+++ db.py
@@ -2,13 +2,15 @@ import mysql.connector
 
 DB_HOST = "127.0.0.1"
 DB_USER = "root"
-DB_PASSWORD = "12345"
+DB_PASSWORD = "rootpass"
 DB_NAME = "sten_sax_pase"
+DB_PORT = "3307"
 
 
 def get_server_connection():
     return mysql.connector.connect(
         host=DB_HOST,
+        port=DB_PORT,
         user=DB_USER,
         password=DB_PASSWORD
     )
@@ -20,6 +22,7 @@ def get_connection():
 
     return mysql.connector.connect(
         host=DB_HOST,
+        port=DB_PORT,
         user=DB_USER,
         password=DB_PASSWORD,
         database=DB_NAME
